1a137a84b0e65232cfea96a64f98429f46bc8c17
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-07-30 15:41:22 -0300

    funcionando

--- nfse-python/nfse-srv-python/app.py
+++ nfse-python/nfse-srv-python/app.py
@@ -1,20 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 import os
 import processing_lab
 import base64
 from imageio import imread
 import io
 import sklearn
+import logging
 import numpy as np
 from tensorflow.keras.models import load_model
 import pickle
 from cfenv import AppEnv
 from sap import xssec
-import json
+from cf_logging import flask_logging
+
 
 #
 app = Flask(__name__)
 env = AppEnv()
+flask_logging.init(app, logging.INFO)
 #
 app.config['DEBUG'] = True
 UPLOAD_FOLDER = r'E:\Users\Daniel\OneDrive\CaptchaML\templates'
@@ -34,6 +37,7 @@ with open(MODEL_LABELS_FILENAME, "rb") as f:
 
 # Load the trained neural network
 model = load_model(MODEL_FILENAME)
+uaa_service = env.get_service(label='xsuaa').credentials
 #
 @app.route('/')
 def home():
@@ -42,58 +46,68 @@ def home():
 #
 @app.route('/ocr', methods=['POST'])
 def predict_text():
-    uaa_service = env.get_service(label='xsuaa').credentials
-   
-    access_token = request.headers.get('authorization')[7:]
-
+    logger = logging.getLogger('route.logger')
+    logger.info('Someone accessed us')
+    if 'Authorization' not in request.headers:
+        logger.info('Authorization not found')
+        abort(403)
+        # return {'message':'Missing authorization key !!'}
+    # scopes =['nfse_admin','nfse_foreigncall','.nfse_admin','.nfse_foreigncall',
+    #          'nfse!t2116.nfse_admin','nfse!t2116.nfse_foreigncall',
+    #          'nfse!t2116']
+    access_token = request.headers.get('Authorization')[7:]
+    logger.info(access_token)
+    logger.info(uaa_service)
     security_context = xssec.create_security_context(access_token, uaa_service)
+    isAuthorized = security_context.check_scope('nfse!t2116.nfse_admin')
+    # for scope in scopes:
+    #     isAuthorized = security_context.check_scope(scope)
+    #     logger.info(isAuthorized)
 
-
-    if 'authorization' not in request.headers:
-
-        print('Missing authorization key !!')
-    isAuthorized = security_context.check_scope('openid')
+    logger.info('Authorization found')
+    logger.info(isAuthorized)
     if not isAuthorized:
-        print('Unauthorized')
-    else:
-        try:
-            # FILE
-            # name1 = request.files['file']
-            # b64_string = base64.b64encode(name1.read())
+        logger.info('Unauthorized !!')
+        abort(403)
+        # return {'message':'Unauthorized !!'}
+    logger.info('Authorization successful')
+    try:
+        # FILE
+        # name1 = request.files['file']
+        # b64_string = base64.b64encode(name1.read())
 
-            # B64 STRING
-            name1 = request.form['string']
-            b64_string = name1
+        # B64 STRING
+        name1 = request.form['string']
+        b64_string = name1
 
-            img = imread(io.BytesIO(base64.b64decode(b64_string)))
-            predictions = []
-            raw_img = processing_lab.process_1(img)
-            img = processing_lab.get_letters(raw_img)
+        img = imread(io.BytesIO(base64.b64decode(b64_string)))
+        predictions = []
+        raw_img = processing_lab.process_1(img)
+        img = processing_lab.get_letters(raw_img)
 
-            for letter in img:
-                # rgb = cv2.cvtColor(letter, cv2.COLOR_BGR2RGB)
-                # Re-size the letter image to 20x20 pixels to match training data
-                letter_image = processing_lab.resize_to_fit(letter, 20, 20)
+        for letter in img:
+            # rgb = cv2.cvtColor(letter, cv2.COLOR_BGR2RGB)
+            # Re-size the letter image to 20x20 pixels to match training data
+            letter_image = processing_lab.resize_to_fit(letter, 20, 20)
 
-                # Turn the single image into a 4d list of images to make Keras happy
-                letter_image = np.expand_dims(letter_image, axis=2)
-                letter_image = np.expand_dims(letter_image, axis=0)
+            # Turn the single image into a 4d list of images to make Keras happy
+            letter_image = np.expand_dims(letter_image, axis=2)
+            letter_image = np.expand_dims(letter_image, axis=0)
 
-                # Ask the neural network to make a prediction
-                prediction = model.predict(letter_image)
+            # Ask the neural network to make a prediction
+            prediction = model.predict(letter_image)
 
-                # Convert the one-hot-encoded prediction back to a normal letter
-                letter = lb.inverse_transform(prediction)[0]
-                predictions.append(letter)
+            # Convert the one-hot-encoded prediction back to a normal letter
+            letter = lb.inverse_transform(prediction)[0]
+            predictions.append(letter)
 
-                # Get captcha's text
-                captcha_text = "".join(predictions)
+            # Get captcha's text
+            captcha_text = "".join(predictions)
 
-            # return {'Predicted': captcha_text}
-            return {'Predicted':captcha_text,'uaa_service':uaa_service,
-                    'access_token':access_token,'security_context':security_context}
-        except:
-            return 'Captcha error !!'
+        # return {'Predicted': captcha_text}
+        return {'Predicted':captcha_text}
+    except:
+        return 'Captcha error !!'
 
 
 #

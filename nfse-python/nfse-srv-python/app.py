757192afc7f696d3a1cb1f2721b23b2e7063fea7
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-09-17 10:53:00 -0300

    new version

--- nfse-python/nfse-srv-python/app.py
+++ nfse-python/nfse-srv-python/app.py
@@ -12,6 +12,7 @@ import pickle
 from cfenv import AppEnv
 from sap import xssec
 from cf_logging import flask_logging
+# import pytesseract
 
 
 #
@@ -30,13 +31,21 @@ app.secret_key = "secret key"
 #
 MODEL_FILENAME = "nfse-srv-python/result_model_letter.h5"
 MODEL_LABELS_FILENAME = "nfse-srv-python/model_labels.dat"
+MODEL_CLASSIFICATION_FILENAME = "nfse-srv-python/captcha_classification_model.hdf5"
+MODEL_CLASSIFICATION_LABELS_FILENAME = "nfse-srv-python/model_classification_labels.dat"
+# pytesseract.pytesseract.tesseract_cmd = 'nfse-srv-python/tessdata/bin/tesseract'
 
 # Load up the model labels (so we can translate model predictions to actual letters)
 with open(MODEL_LABELS_FILENAME, "rb") as f:
     lb = pickle.load(f)
 
+with open(MODEL_CLASSIFICATION_LABELS_FILENAME, "rb") as f:
+    lb_class = pickle.load(f)
+
 # Load the trained neural network
 model = load_model(MODEL_FILENAME)
+classification = load_model(MODEL_CLASSIFICATION_FILENAME)
+
 uaa_service = env.get_service(label='xsuaa').credentials
 #
 @app.route('/')
@@ -51,39 +60,54 @@ def predict_text():
     if 'Authorization' not in request.headers:
         logger.info('Authorization not found')
         abort(403)
-        # return {'message':'Missing authorization key !!'}
-    # scopes =['nfse_admin','nfse_foreigncall','.nfse_admin','.nfse_foreigncall',
-    #          'nfse!t2116.nfse_admin','nfse!t2116.nfse_foreigncall',
-    #          'nfse!t2116']
+
     access_token = request.headers.get('Authorization')[7:]
     logger.info(access_token)
     logger.info(uaa_service)
     security_context = xssec.create_security_context(access_token, uaa_service)
-    isAuthorized = security_context.check_scope('nfse!t2116.nfse_foreigncall') #.nfse_admin para teste local
-    # for scope in scopes:
-    #     isAuthorized = security_context.check_scope(scope)
-    #     logger.info(isAuthorized)
+    # escopo PRE: nfse!t2893.nfse_foreigncall
+    # escopo DEV: nfse!t2116.nfse_foreigncall
+    isAuthorized = security_context.check_scope('nfse!t2893.nfse_foreigncall') #.nfse_admin para teste local
 
     logger.info('Authorization found')
     logger.info(isAuthorized)
     if not isAuthorized:
         logger.info('Unauthorized !!')
         abort(403)
-        # return {'message':'Unauthorized !!'}
+
     logger.info('Authorization successful')
     try:
-        # FILE
+        ## FILE
         # name1 = request.files['file']
         # b64_string = base64.b64encode(name1.read())
 
-        # B64 STRING
+        ## B64 STRING
         name1 = request.form['string']
         b64_string = name1
 
+        # read b64 image
         img = imread(io.BytesIO(base64.b64decode(b64_string)))
+
+        # Classify image
+        captcha_class = processing_lab.classify_captcha(img)
+        captcha_image = processing_lab.resize_to_fit(captcha_class, 20, 20)
+        # Turn the single image into a 4d list of images to make Keras happy
+        captcha_image = np.expand_dims(captcha_image, axis=2)
+        captcha_image = np.expand_dims(captcha_image, axis=0)
+        # Ask the neural network to make a prediction
+        prediction = classification.predict(captcha_image)
+        # Convert the one-hot-encoded prediction back to a normal letter
+        captcha_class = int(lb_class.inverse_transform(prediction)[0])
+
         predictions = []
-        raw_img = processing_lab.process_1(img)
-        img = processing_lab.get_letters(raw_img)
+
+        # apply different model depending on the captcha type
+        if captcha_class in [1,2]:
+            img = processing_lab.model1(img)
+        elif captcha_class == 7:
+            img = processing_lab.model2(img)
+        elif captcha_class == 8:
+            img = processing_lab.model3(img)
 
         for letter in img:
             # rgb = cv2.cvtColor(letter, cv2.COLOR_BGR2RGB)
@@ -103,15 +127,15 @@ def predict_text():
 
             # Get captcha's text
             captcha_text = "".join(predictions)
-
-        # return {'Predicted': captcha_text}
-        logger.info(captcha_text)
-        return {'Predicted':captcha_text}
+            captcha_text = captcha_text.replace("_", "")
+            # filename = name1.filename
+            # Find real captcha name
+            # end = filename.rfind('.')  # last occurence of '.'
+            # real = filename[:end]
+        return {'Predicted': captcha_text}
     except:
-        return 'Captcha error !!'
+        return {'Predicted': 'error!!'}
 
-
-#
 if __name__ == '__main__':
     # app.run(debug=True, use_debugger=False, use_reloader=False)
     #port = int(os.getenv("PORT", 8080))

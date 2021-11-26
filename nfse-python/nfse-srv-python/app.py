a31360f7eb9fa2a42e408e972456bd8449c644b0
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-11-26 12:12:32 -0300

    Update app.py

--- nfse-python/nfse-srv-python/app.py
+++ nfse-python/nfse-srv-python/app.py
@@ -213,6 +213,7 @@ def predict_text():
             return {'Predicted': str(captcha)}
         elif key == 'code' and int(name1['id']) != 10:
             return {'REQUEST ERROR': 'Wrong ID for key CODE!! ID must be 10 but found value: ' + str(name1['id'])}
+
     except Exception as e:
         logger.info('ERROR MSG')
         logger.info(e)
@@ -224,7 +225,7 @@ def predict_text():
             return {'REQUEST ERROR': 'ID key passed with no value inside!!'}
 
         elif int(name1['id']) not in ALLOWED_CAPTCHAS:  # not passing an valid ID
-            return {'REQUEST ERROR': 'Captcha ID not allowed!! Verify ID value'}
+            return {'REQUEST ERROR': 'Captcha ID not allowed, verify ID value!! Passed ID was: ' + str(name1['id'])}
 
         else:
             return {'PYTHON ERROR': 'There was an error reading the image, check python log and b64 string!!',
